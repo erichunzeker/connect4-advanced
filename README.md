691400027c5a1d1ab3ad097d3775c3b26b40bdf4
Todd Waits <twaits@sei.cmu.edu>
2018-11-05 15:11:02 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Connect 4 II: The Revenge-- <Replace with your name>
+
+Name: <Full Name>
+Pitt ID: <ID>
+
+## Installation
+
+1. Instructions to get your code up and running.
---
+++ connect4.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from models import db
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    app.root_path, "connect4.db"
+)
+# Suppress deprecation warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+
+# CLI Commands
+@app.cli.command("initdb")
+def init_db():
+    """Initializes database and any model objects necessary for assignment"""
+    db.drop_all()
+    db.create_all()
+
+
+@app.cli.command("devinit")
+def init_dev_data():
+    """Initializes database with data for development and testing"""
+    db.drop_all()
+    db.create_all()
+
+
+if __name__ == "__main__":
+    app.run()
